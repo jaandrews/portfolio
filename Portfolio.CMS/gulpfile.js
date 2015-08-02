d0189a5e2bc97378ddcbf5d3e49cebf6e2aabdef
JesseA <jesse@bonsaimediagroup.com>
2015-08-01 19:28:24 -0700

    set up project

---
+++ Portfolio.CMS/gulpfile.js
@@ -0,0 +1,25 @@
+﻿/// <binding ProjectOpened='sass:watch' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass({
+            sourceComments: 'map',
+            sourceMap: 'sass',
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie 10']
+        }))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
